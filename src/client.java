5f3fec27036934f235c41499acaaeaee8f3fabc2
Ryan <jmacrae@clemson.edu>
2017-02-18 13:08:46 -0500

    update client

--- src/client.java
+++ src/client.java
@@ -7,7 +7,10 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 
 /**
- * Created by ryan on 2/10/17.
+ * Ryan MacRae, Tyler Brown
+ * February 18, 2017
+ * CPSC 3600
+ * Programming Assignment 4
  */
 public class client {
     public static void main(String[] args) throws IOException {
@@ -17,13 +20,22 @@ public class client {
         System.out.println();
         System.out.println("Please specify the port number: ");
         BufferedReader soc = new BufferedReader(new InputStreamReader(System.in));
-        Integer sock = soc.read();
+        Integer sock = Integer.valueOf(soc.readLine());
         System.out.println();
         System.out.println("Please specify the 1 for GET, 2 for PUT");
         BufferedReader choice = new BufferedReader(new InputStreamReader(System.in));
-        Integer ch = choice.read();
+        Integer ch = Integer.valueOf(choice.readLine());
+        String action;
+        if (ch.equals(1)){
+            action = "GET";
+        }
+        else {
+            action = "PUT";
+        }
         System.out.println();
-        System.out.println("Please specify the file on the remote server that you want to ");
+        System.out.println("Please specify the file on the remote server that you want to GET/PUT: ");
+        BufferedReader fil = new BufferedReader(new InputStreamReader(System.in));
+        String file = fil.readLine();
         Socket clientSocket = null;
         try {
             clientSocket = new Socket(serv, sock);
@@ -33,7 +45,7 @@ public class client {
 
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        out.println("GET /index.html HTTP/1.0\n");
+        out.println("GET /" + file + " HTTP/1.0\n");
         while (in.readLine() != null) {
             System.out.println(in.readLine());
         }
